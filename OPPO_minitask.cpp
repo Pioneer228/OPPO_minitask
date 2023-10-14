ec5e7c6ccc5f88a51d868181156d21ccef038ed6
Vadim_Egunko <daad300604@gmail.com>
2023-10-14 09:23:34 +0700

    ...

--- OPPO_minitask.cpp
+++ OPPO_minitask.cpp
@@ -2,54 +2,56 @@
 #include <fstream>
 #include <string>
 #include <vector>
-using namespace std;
-
 
 struct Time {
-	int MM;
-	int HH;
+    int MM;
+    int HH;
 
-	Time(int m, int h) : MM(m), HH(h) {}
+    Time(int m, int h) : MM(m), HH(h) {}
 };
 
 struct Menu {
-	string Name;
-	string Price;
-	Time menuTime;
+    std::string Name;
+    std::string Price;
+    Time menuTime;
 
-	Menu(const string& n, const string& p, const Time& t)
-		: Name(n), Price(p), menuTime(t) {}
+    Menu(const std::string& n, const std::string& p, const Time& t)
+        : Name(n), Price(p), menuTime(t) {}
 };
 
+std::vector<Menu> txtToStruct(std::ifstream& in) {
+    std::string input;
+    std::vector<Menu> arr;
+    while (std::getline(in, input, ' ')) {
+        std::string Name = input;
+
+        if (std::getline(in, input, ' ')) {
+            int MM, HH;
+            char colon = ':';
+            std::string Price = input;
+
+            if (in >> MM >> colon >> HH) {
+                arr.emplace_back(Name, Price, Time(MM, HH));
+                in.ignore();
+            }
+        }
+    }
+    return arr;
+}
+
+void printStruct(const std::vector<Menu>& arr) {
+    for (const Menu& dish : arr) {
+        std::cout << "Название блюда: " << dish.Name << std::endl;
+        std::cout << "Цена: " << dish.Price << std::endl;
+        std::cout << "Время приготовления: " << dish.menuTime.MM << ":" << dish.menuTime.HH << std::endl;
+    }
+}
+
 int main() {
-	setlocale(LC_ALL, "Russian");
-	ifstream in("Menu.txt");
-	vector<Menu> mass;
-	string input;
-
-	while (getline(in, input, ' ')) {
-		string Name = input;
-
-		if (getline(in, input, ' ')) {
-			int MM, HH;
-			char colon = ':';
-			string Price = input;
-
-			if (in >> MM >> colon >> HH) {
-				mass.emplace_back(Name, Price, Time(MM, HH));
-				in.ignore();
-			}
-		}
-
-	}
-
-	for (Menu& dish : mass) {
-		cout << "Название блюда: " << dish.Name << endl;
-		cout << "Цена: " << dish.Price << endl;
-		cout << "Время приготовления: " << dish.menuTime.MM << ":" << dish.menuTime.HH << endl;
-	}
-
-	in.close();
-
+    setlocale(LC_ALL, "Russian");
+    std::ifstream in("Menu.txt");
+    std::vector<Menu> arr = txtToStruct(in);
+    printStruct(arr);
+    in.close();
 
 }
