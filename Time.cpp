e1b901f0d5d664b8f607e6b85f811de11195fea3
Vadim_Egunko <daad300604@gmail.com>
2023-11-25 09:43:11 +0700

    Рефакторинг

--- Time.cpp
+++ Time.cpp
@@ -6,6 +6,7 @@
 void Time::SetMinutes(int minutes) {
 	mm = minutes;
 }
+
 void Time::SetHours(int hours) {
 	hh = hours;
 }
@@ -13,6 +14,7 @@ void Time::SetHours(int hours) {
 int Time::GetMinutes() const {
 	return mm;
 }
+
 int Time::GetHours() const {
 	return hh;
 }
