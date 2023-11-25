f514d34a81e266b2f2bb9d02463de421e2ff88b8
Vadim_Egunko <daad300604@gmail.com>
2023-11-25 09:41:11 +0700

    Рефакторинг

--- Time.cpp
+++ Time.cpp
@@ -3,10 +3,18 @@
 #include "Time.h"
 
 
-void Time::SetMinutes(int minutes) { mm = minutes; }
-void Time::SetHours(int hours) { hh = hours; }
+void Time::SetMinutes(int minutes) {
+	mm = minutes;
+}
+void Time::SetHours(int hours) {
+	hh = hours;
+}
 
-int Time::GetMinutes() const { return mm; }
-int Time::GetHours() const { return hh; }
+int Time::GetMinutes() const {
+	return mm;
+}
+int Time::GetHours() const {
+	return hh;
+}
 
 Time::Time(const int minutes, const int hours) : mm(minutes), hh(hours) {}
